16c32b204a4da74eb7fabcf16623d96f1706b4ad
keren0908 <akayayy@gmail.com>
2017-10-03 19:01:52 -0400

    init program

---
+++ config.py
@@ -0,0 +1,6 @@
+DEBUG = True # Turns on debugging features in Flask
+BCRYPT_LEVEL = 12 # Configureation for the Flask-Bcrypt extension
+MAIL_FROM_EMAIL = "robert@example.com"  ## defalt"from" address for email
+
+
+
